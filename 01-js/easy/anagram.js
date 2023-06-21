f46f8fe09ef4ab6539f0c474db20f8877d8763c6
Replit user <>
2023-06-21 18:10:13 +0000

    anagram done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
+	if(str1.length != str2.length){
+		return false;
+	}
+	s1=str1.split("").sort().join("");
+	s2=str2.split("").sort().join("");
 
+	if(s1 === s2){
+		return true;
+	}
+	return false;
 }
-
 module.exports = isAnagram;
